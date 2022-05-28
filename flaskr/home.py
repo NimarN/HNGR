15637036a68c267d59e6980d64649429a73016ca
NimarN <nimarnasir1128@gmail.com>
2022-05-28 08:22:59 -0400

    initial commit

---
+++ flaskr/home.py
@@ -0,0 +1,11 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+) 
+
+bp = Blueprint('home', __name__, url_prefix='/') 
+
+@bp.route('', methods=('GET', 'POST'))
+def index():
+    return render_template('home/index.html')
